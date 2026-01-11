738991f7e850f219e60f4e080d0e945de4411511
Vitek-1 <vitfrolik@gmail.com>
2026-01-11 17:49:38 +0100

    vytvoreni tridy Room a jejích metod

---
+++ Rooms/Room.java
@@ -0,0 +1,60 @@
+import java.util.HashMap;
+
+public class Room {
+    private String name;
+    private String description;
+
+    private HashMap<String, Room> exits;
+    private HashMap<String, Item> items;
+    private HashMap<String, Character> characters;
+
+    private boolean locked;
+
+    public Room(String name, String description) {
+
+    }
+
+    public String getName() {
+        return null;
+    }
+
+    public String getDescription() {
+        return null;
+    }
+
+    public void addExit(String name, Room room) {
+
+    }
+
+    public Room getExit(String name) {
+        return null;
+    }
+
+    public void addItem(Item item) {
+
+    }
+
+    public Item getItem(String name) {
+        return null;
+    }
+
+    public void removeItem(String name) {
+
+    }
+
+    public void addCharacter(Character character) {
+
+    }
+
+    public Character getCharacter(String name) {
+        return null;
+    }
+
+    public boolean isLocked() {
+        return false;
+    }
+
+    public void unlock() {
+
+    }
+}
