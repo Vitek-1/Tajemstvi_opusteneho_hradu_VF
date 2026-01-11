de7a330d8ed4249c19af385160d6d59e10b05179
Vitek-1 <vitfrolik@gmail.com>
2026-01-11 17:40:28 +0100

    Vytvoreni tridy Console.java pro zacatek hry, vytvoreni mistnosti a hrace

---
+++ src/Console.java
@@ -0,0 +1,23 @@
+import java.util.HashMap;
+
+public class Console {
+    private Player player;
+    private HashMap<String, Room> rooms;
+    private boolean running;
+
+    public Console(){}
+
+    private void createRooms(){}
+
+    private void createPlayer(){}
+
+    public void start(){}
+
+    public void endGame(boolean victory){}
+
+    private void checkVictory(){}
+
+    public Player getPlayer(){
+        return null;
+    }
+}
