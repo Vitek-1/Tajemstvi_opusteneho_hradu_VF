f880a5a8d225e0a2c66344f97cf711272a207cc7
Vitek-1 <vitfrolik@gmail.com>
2026-01-11 14:24:58 +0100

    Vytvoření třídy Command.java a potřebných metod

---
+++ Command/Command.java
@@ -0,0 +1,12 @@
+public abstract class Command {
+
+    protected String prikaz;
+
+    public void setPrikaz(String prikaz) {
+        this.prikaz = prikaz;
+    }
+
+    public abstract String execute();
+
+    public abstract boolean exit();
+}
