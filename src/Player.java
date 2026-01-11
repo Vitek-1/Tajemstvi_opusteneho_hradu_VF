d07ec3b238a1041403fdc8f52ae4b25d2d370206
Vitek-1 <vitfrolik@gmail.com>
2026-01-11 17:39:24 +0100

    Vytvoreni tridy hrace pro tvormu hrace

---
+++ src/Player.java
@@ -0,0 +1,28 @@
+public class Player {
+    private Room currentRoom;
+    private Inventar inventar;
+    private boolean alive;
+
+    public Player(Room startRoom) {
+    }
+
+    public Room getCurrentRoom() {
+        return null;
+    }
+
+    public void setCurrentRoom(Room room) {
+
+    }
+
+    public Inventar getInventory() {
+        return null;
+    }
+
+    public boolean isAlive() {
+        return true;
+    }
+
+    public void die() {
+    }
+
+}
