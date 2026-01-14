024c2db85ff6867de70b1d0f2d1d7d0270df4cbe
Vitek-1 <vitfrolik@gmail.com>
2026-01-14 20:56:04 +0100

    Changing abstract class to normal class

---
+++ src/Item.java
@@ -0,0 +1,20 @@
+public class Item {
+    private String name;
+    private String description;
+    private boolean owned;
+
+
+    public Item(String name, String description, boolean owned) {
+        this.name = name;
+        this.description = description;
+        this.owned = owned;
+    }
+
+    public void itemCreating(){
+
+    }
+
+    public void use(Player player, Console game){
+
+    }
+}
