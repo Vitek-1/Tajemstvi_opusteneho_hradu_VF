9c2b3104a334e35b7ac87628343d10fef4a40782
Vitek-1 <vitfrolik@gmail.com>
2026-01-16 09:59:16 +0100

    Přejmenování na lepší název

---
+++ src/Characters/Ghost_Armorer.java
@@ -0,0 +1,16 @@
+package Characters;
+
+import Command.Console;
+import Hrac.Player;
+
+public class Ghost_Armorer extends Character {
+
+    public Ghost_Armorer(String name, String description) {
+        super(name, description);
+    }
+
+    @Override
+    public void talk(Player player, Console game) {
+
+    }
+}
