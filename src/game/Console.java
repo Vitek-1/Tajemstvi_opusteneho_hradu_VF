0fa44b1a3a0b0c70b66bdecce2528bc5d6eb871f
Vitek-1 <vitfrolik@gmail.com>
2026-01-22 06:34:01 +0100

    Vytvoreni prikazove radky a start celeho programu

--- src/game/Console.java
+++ src/game/Console.java
@@ -1,15 +1,54 @@
 package game;
 
+import command.*;
+
 import java.util.HashMap;
+import java.util.Scanner;
 
 public class Console{
     private Player player;
-    private HashMap<String, Room> rooms;
+    private GameData gameData;
     private boolean running;
+    private HashMap<String, Command> mapa = new HashMap<>();
+    private Scanner scanner = new Scanner(System.in);
 
     public Console(){}
 
-    public void start(){}
+    private void inicializace() {
+        mapa.put("jdi", new Jdi(this.player, this.gameData));
+        mapa.put("inventar", new Inventar());
+        mapa.put("mluv", new Mluv());
+        mapa.put("pouzij", new Pouzij());
+        mapa.put("prozkumej", new Prozkoumej());
+        mapa.put("vezmi", new Vezmi());
+    }
+
+    private void proved() {
+        System.out.print(">> ");
+        String prikaz = scanner.next();
+        prikaz = prikaz.trim().toLowerCase();
+        if (mapa.containsKey(prikaz)) {
+            System.out.println(">> " + mapa.get(prikaz).execute());
+            running = mapa.get(prikaz).exit();
+        } else {
+            System.out.println(">> Nedefinovany prikaz");
+        }
+    }
+
+    public void start() {
+        gameData = GameData.loadGameDataFromResources("/Rooms.json");
+        Room startovni_room = gameData.findRoomById("nadvori");
+        Inventar startovniInventar = new Inventar();
+        this.player = new Player(startovni_room, startovniInventar,true);
+        inicializace();
+        try {
+            do {
+                proved();
+            } while (!running);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
 
     public void endGame(boolean victory){}
 
