0b03bb78667296794d39b94585fde911badd9fe5
Vitek-1 <vitfrolik@gmail.com>
2026-01-24 21:55:53 +0100

    Dopsání metod execute, exit,

--- src/command/Mluv.java
+++ src/command/Mluv.java
@@ -1,9 +1,40 @@
 package command;
 
+import characters.Ghost_Armorer;
+import characters.Ghost_King;
+import characters.Ghost_librarian;
+import game.GameData;
+import game.Player;
+
 public class Mluv extends Command {
+
+    private Player player;
+    private Ghost_Armorer armorer;
+    private Ghost_King king;
+    private Ghost_librarian librarian;
+    private GameData gameData;
+
+    public Mluv(Player player, Ghost_Armorer armorer, Ghost_King king, Ghost_librarian librarian, GameData gameData) {
+        this.player = player;
+        this.armorer = armorer;
+        this.king = king;
+        this.librarian = librarian;
+        this.gameData = gameData;
+    }
 
     @Override
     public String execute() {
+        switch (player.getCurrentRoom().getId()) {
+            case "nadvori":
+                armorer.talk(player, gameData);
+                break;
+            case "hrad_sin":
+                king.talk(player, gameData);
+                break;
+            case "knihovna":
+                librarian.talk(player, gameData);
+                break;
+        }
         return "";
     }
 
