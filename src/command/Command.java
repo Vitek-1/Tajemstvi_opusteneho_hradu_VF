f3b02ec045a6fab091a4effd73cb800803680bfe
Vitek-1 <vitfrolik@gmail.com>
2026-01-24 13:42:43 +0100

    Smazáni nadbytečné metody

--- src/command/Command.java
+++ src/command/Command.java
@@ -4,10 +4,6 @@ public abstract class Command {
 
     protected String prikaz;
 
-    public void setPrikaz(String prikaz) {
-        this.prikaz = prikaz;
-    }
-
     public abstract String execute();
 
     public abstract boolean exit();
